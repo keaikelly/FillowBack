79b6ed77745dc8609d1eb94a26cfd7a8eb664c18
keaikelly <keaikelly@naver.com>
2026-02-24 12:21:42 +0900

    [init] 초기구축

---
+++ src/main/java/com/fillow/FillowApplication.java
@@ -0,0 +1,13 @@
+package org.example.fillow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FillowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FillowApplication.class, args);
+    }
+
+}
