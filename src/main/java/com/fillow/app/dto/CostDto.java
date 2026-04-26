42bac903967199307e79646e45880a32d83e86c9
keaikelly <keaikelly@naver.com>
2026-04-26 15:39:35 +0900

    [feat] Cost Dto

---
+++ src/main/java/com/fillow/app/dto/CostDto.java
@@ -0,0 +1,43 @@
+package com.fillow.app.dto;
+
+import com.fillow.domain.entity.Cost;
+import com.fillow.domain.enums.cost.CostType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+public class CostDto {
+
+    //비용생성요청
+    @Getter
+    @NoArgsConstructor
+    public static class CostCreateRequest {
+        private Long SimulId;
+        private CostType costType;
+        private String costName;
+        private BigDecimal amount;
+    }
+
+    //비용응답
+    @Getter
+    @Builder
+    public static class CostResponse {
+        private Long costId;
+        private Long simulId;
+        private CostType costType;
+        private String costName;
+        private BigDecimal amount;
+
+        public static CostResponse from(Cost cost) {
+            return CostResponse.builder()
+                    .costId(cost.getCostId())
+                    .simulId(cost.getSimulation().getSimulId())
+                    .costType(cost.getCostType())
+                    .costName(cost.getCostName())
+                    .amount(cost.getAmount())
+                    .build();
+        }
+    }
+}
