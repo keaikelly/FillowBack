05537a109279340dc6ae40a044403bcb461b233f
keaikelly <keaikelly@naver.com>
2026-05-21 19:40:21 +0900

    [feat] jwt 필터 기본 설정

---
+++ src/main/java/com/fillow/config/JwtAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.fillow.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    //API마다 JWT 확인 필터 기능
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        // 프론트에서 받은 Authorization 헤더 가져오기
+        String bearerToken = request.getHeader("Authorization");
+
+        // Bearer 토큰인지 확인
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+
+            // "Bearer " 제거
+            String token = bearerToken.substring(7);
+
+            // 토큰 검증
+            if (jwtTokenProvider.validateToken(token)) {
+
+                String loginId = jwtTokenProvider.getLoginId(token); //jwt내 loginId 꺼냄
+
+                System.out.println("JWT 인증 성공: " + loginId);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/fillow/config/SecurityConfig.java
+++ src/main/java/com/fillow/config/SecurityConfig.java
@@ -1,14 +1,46 @@
 package com.fillow.config;
 
-import jakarta.persistence.Column;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); //비밀번호 암호화 도구를 스프링에 등록
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) //jwt이므로 csrf 허용제거
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )//jwt이므로 세션 저장 없음
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/users/register",
+                                "/api/users/login",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                                ).permitAll() //위 api는 토큰없이 가능
+                        .anyRequest().authenticated() //나머지는 전부 인가 필요
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter, //요청들어오면 jwt 인증부터
+                        UsernamePasswordAuthenticationFilter.class
+                );
+        return http.build();
     }
 }
